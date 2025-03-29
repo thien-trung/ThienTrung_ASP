7719abafeab565a24707144e36ddbec1330b74c5
thien-trung <thientrung.tran756@gmail.com>
2025-03-29 08:40:33 +0700

    buoi2

---
+++ Model/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace TranThienTrung2122110179.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public int Image { get; set; }
+
+    }
+}
